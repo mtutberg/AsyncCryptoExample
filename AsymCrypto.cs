8278424c05509cb03d1b424c23ae9fc21addd521
mtutberg <136358682+mtutberg@users.noreply.github.com>
2023-06-12 08:56:07 -0700

    Add files via upload

---
+++ AsymCrypto.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsymCryptoExample
+{
+    internal static class AsymCrypto
+    {
+        public static void GenerateKey()
+        {
+            RSA rsa = RSA.Create();
+            RSAParameters rsaparameters = rsa.ExportParameters(true); //Generate Private and Public Keys
+            File.WriteAllBytes("Modulus.txt", rsaparameters.Modulus); //Write Public Key to File
+            File.WriteAllBytes("Exponent.txt",rsaparameters.Exponent); //Write Public Key to File
+            File.WriteAllBytes("D.txt", rsaparameters.D); //Write Private Key to File
+            File.WriteAllBytes("P.txt", rsaparameters.P); //Write Private Key to File
+            File.WriteAllBytes("Q.txt", rsaparameters.Q); //Write Private Key to File
+            File.WriteAllBytes("DP.txt", rsaparameters.DP); //Write Private Key to File
+            File.WriteAllBytes("DQ.txt", rsaparameters.DQ); //Write Private Key to File
+            File.WriteAllBytes("InverseQ.txt", rsaparameters.InverseQ); //Write Private Key to File
+            rsa.Dispose();
+        }
+
+        public static void Encrypt(String txt)
+        {
+            RSA rsa = RSA.Create();
+            RSAParameters rsaparameters = new RSAParameters();
+            rsaparameters.Modulus = File.ReadAllBytes("Modulus.txt"); //Read Public Key from File
+            rsaparameters.Exponent = File.ReadAllBytes("Exponent.txt"); //Read Public Key from File
+            rsa.ImportParameters(rsaparameters); //Construct Public Key
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(txt);
+            byte[] encryptedtext = rsa.Encrypt(buffer, RSAEncryptionPadding.Pkcs1); //Encrypt text using Public Key
+            rsa.Dispose();
+            File.WriteAllBytes("Encrypted.txt", encryptedtext);
+        }
+
+        public static String Decrypt()
+        {
+            RSA rsa = RSA.Create();
+            RSAParameters rsaparameters = new RSAParameters();
+            rsaparameters.Modulus = File.ReadAllBytes("Modulus.txt"); //Read Public Key from File
+            rsaparameters.Exponent = File.ReadAllBytes("Exponent.txt"); //Read Public Key from File
+            rsaparameters.D = File.ReadAllBytes("D.txt"); //Read Private Key from File
+            rsaparameters.P = File.ReadAllBytes("P.txt"); //Read Private Key from File
+            rsaparameters.Q = File.ReadAllBytes("Q.txt"); //Read Private Key from File
+            rsaparameters.DP = File.ReadAllBytes("DP.txt"); //Read Private Key from File
+            rsaparameters.DQ = File.ReadAllBytes("DQ.txt"); //Read Private Key from File
+            rsaparameters.InverseQ = File.ReadAllBytes("InverseQ.txt"); //Read Private Key from File
+            rsa.ImportParameters(rsaparameters); //Construct Private Key
+            byte[] encryptedtext = File.ReadAllBytes("Encrypted.txt");//Retrieve the encrypted text
+            byte[] buffer = rsa.Decrypt(encryptedtext, RSAEncryptionPadding.Pkcs1); //Decrypt the encrypted text
+            rsa.Dispose();
+            String txt = System.Text.Encoding.UTF8.GetString(buffer);
+            return txt;
+        }
+
+        public static String DecryptWithoutPrivateKey()
+        {
+            RSA rsa = RSA.Create();
+            RSAParameters rsaparameters = new RSAParameters();
+            rsaparameters.Modulus = File.ReadAllBytes("Modulus.txt"); //Read Public Key from File
+            rsaparameters.Exponent = File.ReadAllBytes("Exponent.txt"); //Read Public Key from File
+            rsa.ImportParameters(rsaparameters); //Construct Private Key
+            byte[] encryptedtext = File.ReadAllBytes("Encrypted.txt");//Retrieve the encrypted text
+            byte[] buffer = rsa.Decrypt(encryptedtext, RSAEncryptionPadding.Pkcs1); //Decrypt the encrypted text
+            rsa.Dispose();
+            String txt = System.Text.Encoding.UTF8.GetString(buffer);
+            return txt;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AsymCryptoExample;
+
+String txt;
+
+Console.WriteLine("Choose your action:");
+Console.WriteLine("1 - Generate RSA Key");
+Console.WriteLine("2 - Enter your text and encrypt");
+Console.WriteLine("3 - Decrypt encrypted text");
+Console.WriteLine("4 - Decrypt without private key (Will cause error)");
+ConsoleKeyInfo cki = Console.ReadKey();
+Console.WriteLine();
+
+switch (cki.Key)
+{
+    case ConsoleKey.D1:
+        AsymCrypto.GenerateKey();
+        Console.WriteLine("RSA key is generated");
+        break;
+    case ConsoleKey.D2:
+        Console.Write("Enter your text to encrypt: ");
+        txt = Console.ReadLine();
+        AsymCrypto.Encrypt(txt);
+        break;
+    case ConsoleKey.D3:
+        txt = AsymCrypto.Decrypt();
+        Console.Write("Your text was: ");
+        Console.WriteLine(txt);
+        break;
+    case ConsoleKey.D4:
+        txt = AsymCrypto.DecryptWithoutPrivateKey();
+        Console.Write("Your text was: ");
+        Console.WriteLine(txt);
+        break;
+    default:
+
+        break;
+
+}
